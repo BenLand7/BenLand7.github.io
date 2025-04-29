b7d2efdb6187ecbda8a795ae2ed67042cd70adcb
BenLand7 <Landeba23@juniata.edu>
2025-04-28 22:16:10 -0400

    final draft?

---
+++ Style/JS/Videos_Java.js
@@ -0,0 +1,24 @@
+const text = "Ben Landesman";
+const typingSpeed = 100; // Adjust typing speed (ms)
+let index = 0;
+
+function typeEffect() {
+    if (index < text.length) {
+        document.getElementById("typing-text").textContent += text.charAt(index);
+        index++;
+        setTimeout(typeEffect, typingSpeed);
+    }
+}
+window.onload = typeEffect;
+
+const toggle = document.querySelector("#toggle")
+
+toggle.onclick = function(){
+    document.body.style.backgroundColor = "#D34F73";
+}
+const set = document.querySelector("#set")
+
+set.onclick = function(){
+    document.body.style.backgroundColor = "white";
+}
+
